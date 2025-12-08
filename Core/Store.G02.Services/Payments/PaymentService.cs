0642ae96211de3ed42ff73317d1d726f1c34cc4a
macknota <adelmagdymacknota@gmail.com>
2025-12-08 14:00:04 +0200

    Add Payment Service Stripe

--- Core/Store.G02.Services/Payments/PaymentService.cs
+++ Core/Store.G02.Services/Payments/PaymentService.cs
@@ -9,15 +9,15 @@ using System.Threading.Tasks;
 
 namespace Store.G02.Services.Payments
 {
-    public class PaymentService(IBasketRepository _basketRepository) : IPaymentService
-    {
-        public async Task<BasketDto> CreatePaymentIntentAsync(string basketId)
-        {
-            // Calculate Amount = SubTotal + Delivery Method Cost
-            // Send Amount TO stripe (To Create Recipe and Payment Intent)
-            var basket = await _basketRepository.GetBasketAsync(basketId);
+    //public class PaymentService(IBasketRepository _basketRepository) : IPaymentService
+    //{
+    //    public async Task<BasketDto> CreatePaymentIntentAsync(string basketId)
+    //    {
+    //        // Calculate Amount = SubTotal + Delivery Method Cost
+    //        // Send Amount TO stripe (To Create Recipe and Payment Intent)
+    //        var basket = await _basketRepository.GetBasketAsync(basketId);
 
 
-        }
-    }
+    //    }
+    //}
 }
